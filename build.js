39e5e3072c3bfdf4e44a799c8e28e235f44ca3a8
Sut <xepinchan@qq.com>
2025-10-16 09:36:44 +0800

    Update build.js

--- build.js
+++ build.js
@@ -26,8 +26,13 @@ function generateUserscriptHeader() {
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand
+// @grant        GM_xmlhttpRequest
+// @updateURL    https://github.com/SutChan/douyin_tool/raw/main/dist/douyin_ui_customizer.user.js
+// @downloadURL  https://github.com/SutChan/douyin_tool/raw/main/dist/douyin_ui_customizer.user.js
 // @license      ${pkg.license}
-// ==/UserScript==`;
+// ==/UserScript==
+/* eslint-disable */
+// @ts-nocheck`;
 }
 
 // 读取文件内容
